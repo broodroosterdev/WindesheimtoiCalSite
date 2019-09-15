2a33dcdcc473dfa057781c826fdb7202ae6c5ce2
broodroosterdev <31385368+broodroosterdev@users.noreply.github.com>
2019-09-15 20:45:18 +0200

    Autocompleting classcodes and klascode checking

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+const main = document.getElementById('main');
+var codes = [];
 
+const init = async () => {
+    const res = await fetch('../data/classcodes.json');
+    codes = await res.json();
+}
 // Search classcodes.json and filter it
 const searchClasscodes = async searchText => {
-    const res = await fetch('../data/classcodes.json');
-    const codes = await res.json();
     //console.log(searchText);
     // Get matches to current text input
     let matches = codes.filter(code => {
@@ -16,12 +20,39 @@ const searchClasscodes = async searchText => {
     }
     outputHtml(matches);
     if (matches.length === 0) {
+        const regexCase = new RegExp(`^${searchText}`, 'gi');
+        let newMatches = codes.filter(code => {
+            return code.id.match(regexCase) || code.code.match(regexCase) || code.klasnaam.match(regexCase);
+        });
+        if (newMatches.length == 1) {
+            search.value = newMatches[0].code
+        }
         return false;
     } else if (matches.length > 0 && searchText !== "") {
+        if(await correctCasing(searchText)){
+            search.value = await correctCasing(searchText)
+        }
         return true;
     }
 }
 
+const checkCasing = async classcode => {
+    const regex = new RegExp(`(?:^|\W)${classcode}(?:$|\W)`, 'g');
+    let correctCases = codes.filter(code => {
+        return code.id.match(regex) || code.code.match(regex) || code.klasnaam.match(regex);
+    })
+    return correctCases.length > 0
+}
+const correctCasing = async classcode => {
+    const regex = new RegExp(`(?:^|\W)${classcode}(?:$|\W)`, 'gi');
+    let correctCases = codes.filter(code => {
+        return code.id.match(regex) || code.code.match(regex) || code.klasnaam.match(regex);
+    })
+    if(correctCases[0]){
+        return correctCases[0].code
+    }
+    
+}
 //Show results in HTML
 const outputHtml = matches => {
     if (matches.length > 0) {
@@ -31,10 +62,7 @@ const outputHtml = matches => {
                 <span class="text-primary">${match.code}</span>
             </div>
         `).join('');
-
         matchList.innerHTML = html;
-
-
     }
 }
 
@@ -43,12 +71,19 @@ const selectCard = code => {
     search.value = code;
 }
 //Checks if input is valid and redirects to api
-const checkSubmit = key => {
+const checkSubmit = async key => {
     if (key == 13) {
-        if (searchClasscodes(search.value)) {
+        if (searchClasscodes(search.value) && await correctCasing(search.value)) {
+            search.value = await correctCasing(search.value)
             window.location.replace(`https://www.broodrooster.dev/windesheim/api/${search.value}`);
+        } else if (searchClasscodes(search.value)) {
+            alert("Ik kan deze klascode niet vinden. Is de klascode correct?")
         }
     }
+}
+
+const showThanks = () => {
+    main.innerHTML
 }
 
 function copyTextToClipboard(text) {
@@ -108,11 +143,13 @@ function copyTextToClipboard(text) {
 
     document.body.removeChild(textArea);
 }
-const copyURL = () => {
-    if (searchClasscodes(search.value)) {
+const copyURL = async () => {
+    if (searchClasscodes(search.value) && await correctCasing(search.value)) {
+        search.value = await correctCasing(search.value)
         copyTextToClipboard(`https://www.broodrooster.dev/windesheim/api/${search.value}`);
+    } else if (searchClasscodes(search.value)) {
+        alert("Ik kan deze klascode niet vinden. Is de klascode correct?");
     }
-
 }
 
 
